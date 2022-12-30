fdb79dd9e0958cfb08d490c89e4d60f7e1cf7cb7
web-software <114349783+web-software@users.noreply.github.com>
2022-12-30 15:43:03 -0600

    4 files modified, 1 added

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['immutable'],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'immutable/no-mutation': 'error',
+  },
+};

eb4158322957a89a6c44f764e2b50388e41fdbbc
web-software <114349783+web-software@users.noreply.github.com>
2022-12-30 14:30:14 -0600

    1 file modified, 2 added

--- api/server.js
+++ api/server.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import { promises as fs } from 'fs';
+import { people } from './people-data';
 
 let app = express();
 
 app.get('/hello', (req, res) => {
   res.send('Hello!');
+});
+
+app.get('/people', (req, res) => {
+  res.json(people);
+});
+
+app.get('/people/:name', (req, res) => {
+  let { name } = req.params;
+
+  let person = people.find((x) => x.name === name);
+
+  res.json(person);
+});
+
+app.get('/file-data', async (req, res) => {
+  let data = await fs.readFile(__dirname + '/people-data.json');
+  let people = JSON.parse(data);
+
+  res.json(people);
 });
 
 app.listen(3000, () => {
